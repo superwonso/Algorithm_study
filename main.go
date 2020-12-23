fe607df59a68e663883ae56469cb45cab8bcc4e4
Cha DongHyeon <chadonghyeon@kakao.com>
2020-12-23 09:54:23 +0900

    20201223

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c float64
+	var count, result, n, i, temp int
+	fmt.Scanf("%f %f %f", &a, &b, &n)
+	c = a / b
+	float64(temp)
+	math.pow(10, n) = temp
+	float64(i)
+	i = c * temp
+	int(i)
+	fmt.Println(i % 10)
+}
+
+//https://www.acmicpc.net/problem/1312
