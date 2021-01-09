baf81dfab24dc0637cfdb068cac1ef924529be67
Cha DongHyeon <chadonghyeon@kakao.com>
2021-01-09 19:32:57 +0900

    20210109

--- c/1312.c
+++ c/1312.c
@@ -6,9 +6,9 @@ double a,b;
 long n;
 scanf("%f %f %d",&a,&b,&n);
 double c=a/b;
-double d=c*pow(10,n);
-double e=floor(d);
-long f=(long)e;
-printf("%d",f%(10));
+double d=c*pow(10,n); // c*(10^n), to extract digit number
+double e=floor(d); // decimal abandon 
+long f=(long)e; // convert to variable "e" integer
+printf("%d",f%(10)); // to extract digit number
 return 0;
 }
