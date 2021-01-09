f3a6966fb871c89b5d7d4d93be50d90f25c25705
Cha DongHyeon <chadonghyeon@kakao.com>
2021-01-09 21:11:52 +0900

    20210109

--- c/1312.c
+++ c/1312.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+int res(int a,int b);
 int main(void)
 {
 double a,b;
 long n;
 scanf("%f %f %d",&a,&b,&n);
 double c=a/b;
-double d=c*pow(10,n); // c*(10^n), to extract digit number
+double d=c*res(10,n); // c*(10^n), to extract digit number
 double e=floor(d); // decimal abandon 
-long f=(long)e; // convert to variable "e" integer
+int f=(int)e; // convert to variable "e" integer
 printf("%d",f%(10)); // to extract digit number
 return 0;
+}
+
+int res(int a,int b)
+{
+if (b==0) return 1;
+else return a*res(a,b-1);
 }
