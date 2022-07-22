dcd6eb5570207261354a0290d3f572d78e55778f
superwonso <chadonghyeon@kakao.com>
2022-07-22 17:22:23 +0900

    Update 15650.go

--- golang/baekjoon/15650/15650.go
+++ golang/baekjoon/15650/15650.go
@@ -1,6 +1,7 @@
 package main
 
 // Solved Not Yet
+// Impleted M=1 only
 import (
 	"fmt"
 	"math/rand"
@@ -12,14 +13,15 @@ func main() {
 	fmt.Scan(&N, &M)
 	result := make([]int, M)
 
-	result[M-1] = rand.Intn(N-1) + 1
-	for i := 0; i < M-1; i++ {
+	result[0] = rand.Intn(N-1) + 1
+	for i := 1; i < M-1; i++ {
 		result[i] = rand.Intn(N-1) + 1
-		if check_overlap(result[i], result[i+1]) {
+		if check_overlap(result[i], result[i-1]) {
 			i--
 		}
 	}
 	sort.Sort(sort.IntSlice(result))
+	println(result)
 	for i := 0; i < M; i++ {
 		fmt.Println(result[i])
 	}
