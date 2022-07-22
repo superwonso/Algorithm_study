17602d02edd2ed624a3c6ddf30f4e6896668b178
superwonso <chadonghyeon@kakao.com>
2022-07-22 14:32:54 +0900

    Solved

--- golang/baekjoon/11053/11053.go
+++ golang/baekjoon/11053/11053.go
@@ -1,35 +1,41 @@
+//Not Solved
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var N, res int
-	A := make([]int, N+1)
-	tmp := make([]int, N+1)
-	for i := 1; i <= N; i++ {
-		fmt.Scanf("%d", &A[i])
-		tmp[i] = 1
+	r := bufio.NewReader(os.Stdin)
+	fmt.Fscanln(r, &N)
+	A := make([]int, N)
+	tmp := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(r, &A[i])
 	}
-	for i := 1; i <= N; i++ {
-		for j := 1; j < i; j++ {
+	for i := 0; i < N; i++ {
+		for j := 0; j < i; j++ {
 			if A[i] > A[j] {
-				max := func(n ...int) int {
-					var x, y int
-					if x > y {
-						return x
-					} else {
-						return y
-					}
-				}
 				tmp[i] = max(tmp[i], tmp[j]+1)
 			}
 		}
 	}
 	res = tmp[0]
-	for i := 1; i <= N; i++ {
+	for i := 0; i < N; i++ {
 		if res < tmp[i] {
 			res = tmp[i]
 		}
 	}
-	fmt.Println(res)
+	fmt.Println(res + 1)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
 }
