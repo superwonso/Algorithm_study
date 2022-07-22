4f6fba3a972d89b1a9570b7107b7431cd692df25
superwonso <chadonghyeon@kakao.com>
2022-07-22 16:12:34 +0900

    Solved

--- golang/baekjoon/12015/12015.go
+++ golang/baekjoon/12015/12015.go
@@ -1,44 +1,49 @@
 package main
 
-// Solved Not Yet
-
 import (
 	"bufio"
 	"fmt"
 	"os"
 )
 
+var lis []int
+var Arr [1000001]int
+
 func main() {
-	var N, idx, temp int = 0, 0, 0
+	var N, l_idx int = 0, 0
 	r := bufio.NewReader(os.Stdin)
 	fmt.Fscanln(r, &N)
-	tmp := make([]int, N+1)
-	for i := 1; i <= N; i++ {
-		fmt.Fscan(r, &temp)
-		if idx == 0 {
-			idx++
-			tmp[idx] = temp
+	for i := 0; i < N; i++ {
+		fmt.Fscan(r, &Arr[i])
+	}
+	lis = push_back(Arr[0])
+	for i := 1; i < N; i++ {
+		if lis[l_idx] < Arr[i] {
+			lis = push_back(Arr[i])
+			l_idx++
 		} else {
-			if tmp[idx-1] < temp {
-				idx++
-				tmp[idx] = temp
-			} else {
-				tmp[lower_bound(tmp, i, temp)] = temp
-			}
+			var idx int = lower_bound(Arr[i])
+			lis[idx] = Arr[i]
 		}
 	}
-	fmt.Println(idx)
+	fmt.Println(len(lis))
 }
 
-func lower_bound(arr []int, n int, key int) int {
-	l, r := 0, len(arr)-1
+func lower_bound(n int) int {
+	var l int = 0
+	var r int = len(lis) - 1
 	for l < r {
-		mid := (l + r) / 2
-		if arr[mid] < n {
-			l = mid + 1
-		} else {
+		var mid int = (l + r) / 2
+		if lis[mid] >= n {
 			r = mid
+		} else {
+			l = mid + 1
 		}
 	}
 	return l
+}
+
+func push_back(n int) []int {
+	lis = append(lis, n)
+	return lis
 }
