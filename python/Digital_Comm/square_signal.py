c40b04363894567e4a71419d8ab0900b84d0c0b2
superwonso <chadonghyeon@kakao.com>
2022-11-29 20:15:20 +0900

    Digital Communication Square Pulse

---
+++ python/Digital_Comm/square_signal.py
@@ -0,0 +1,30 @@
+from scipy import signal
+import matplotlib.pyplot as plot
+import numpy as np
+
+A=1; g=3
+
+# Sampling rate 1000 hz / second
+t = np.linspace(0, 50, 1000, endpoint=True)
+
+# Plot the square wave signal
+plot.plot(t, signal.square(t/g))
+
+# Give a title for the square wave plot
+plot.title('Sqaure wave')
+
+# Give x axis label for the square wave plot
+plot.xlabel('T')
+
+# Give y axis label for the square wave plot
+plot.ylabel('Amplitude')
+plot.grid(True, which='both')
+
+# Provide x axis and line color
+plot.axhline(y=0, color='k')
+
+# Set the max and min values for y axis
+plot.ylim(-2, 2)
+
+# Display the square wave drawn
+plot.show()
