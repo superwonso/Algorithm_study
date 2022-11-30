bf253ba19c4b859f01ce2d4df39d951ef6ab0970
superwonso <chadonghyeon@kakao.com>
2022-11-30 20:58:05 +0900

    Update fft_square_signal.py

--- python/Digital_Comm/fft_square_signal.py
+++ python/Digital_Comm/fft_square_signal.py
@@ -1,31 +1,29 @@
-from scipy import signal 
 import matplotlib.pyplot as plot
 import numpy as np
 
-A=1; g=1
+A=1
 
 # Sampling rate 1000 hz / second
-t = np.linspace(-10, 10, 1000, endpoint=True)
+w = np.linspace(-20, 20, 1000, endpoint=True)
 
-# Calculate the FFT of the square wave signal
-y_tmp = signal.square(t/g)
-fft_y = np.fft.fft(y_tmp)
+# Plot the square wave signal
+plot.plot(w, np.sinc(w/(2*np.pi))*A)
 
-# Plot the fft square wave signal
-plot.plot(t, fft_y)
+# Give a title for the square wave plot
+plot.title('Sqaure wave')
 
 # Give x axis label for the square wave plot
-plot.xlabel('Frequency')
+plot.xlabel('w')
 
 # Give y axis label for the square wave plot
-plot.ylabel('τ')
+plot.ylabel('S(w)')
 plot.grid(True, which='both')
 
 # Provide x axis and line color
 plot.axhline(y=0, color='k')
 
 # Set the max and min values for y axis
-plot.ylim(-6, 6)
+plot.ylim(-1.5, 1.5)
 
 # Display the square wave drawn
 plot.show()
