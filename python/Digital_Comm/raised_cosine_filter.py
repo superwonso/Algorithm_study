b913c8b75c67dacc359ec68c7573e0f4e176edd2
superwonso <chadonghyeon@kakao.com>
2022-11-30 19:23:08 +0900

    Raised Cosine Filter

---
+++ python/Digital_Comm/raised_cosine_filter.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plot
+import numpy as np
+
+r = 0.5 # roll-off factor , r = 0, 0.5, 1
+W_0 = 2; f_delta = 0.5
+
+# Define the raised cosine function
+# P(f)= 1 (abs(f)<=W_0 - f_delta) , (1+cos(pi*abs(f)/2f_delta))/2 (W_0-f_delta<abs(f)<=W_0+f_delta) , 0 (abs(f)>W_0+f_delta)
+def raised_cosine(f, W_0, f_delta):
+    y = np.zeros(len(f))
+    if r == 0: 
+        return np.sinc(f)
+    for i in range(len(f)):
+        if np.abs(f[i]) <= (W_0 - f_delta):
+            y[i] = 1
+        elif (W_0-f_delta) < np.abs(f[i]) <= (W_0+f_delta):
+            y[i] = (1+np.cos(np.pi*np.abs(f[i])/(2*f_delta)))/2
+        else:
+            y[i] = 0
+    return y
+
+# Sampling rate 1000 hz / second
+f = np.linspace(-10, 10, 1000, endpoint=True)
+
+# Plot the square wave signal
+plot.plot(f, raised_cosine(f,W_0,f_delta))
+
+# Give a title for the square wave plot
+plot.title('Raised Cosine Filter')
+
+# Give x axis label for the square wave plot
+plot.xlabel('f')
+
+# Give y axis label for the square wave plot
+plot.ylabel('P(f)')
+plot.grid(True, which='both')
+
+# Provide x axis and line color
+plot.axhline(y=0, color='k')
+
+# Set the max and min values for y axis
+plot.ylim(-3, 3)
+
+# Display the square wave drawn
+plot.show()
+
+
