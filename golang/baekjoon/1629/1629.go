b1a4bed88800889ce6c72bf319657a0d8ff8dbb0
superwonso <chadonghyeon@kakao.com>
2022-12-06 20:21:49 +0900

    1629 Solved(Golang)

--- golang/baekjoon/1629/1629.go
+++ golang/baekjoon/1629/1629.go
@@ -11,21 +11,16 @@ var r = bufio.NewReader(os.Stdin)
 func main() {
 	var a, b, c int = 0, 0, 0
 	fmt.Fscan(r, &a, &b, &c)
-	for i := 0; i < b; i++ {
-		a = modulo_distributive(a, a, c)
-	}
+	a = modulo_distributive(a, b, c)
 	fmt.Println(a)
 }
 
-/*func get_Digits(n int) int {
-	var digit int
-	for n > 0 {
-		digit++
-		n /= 10
-	}
-	return digit
-}*/
-
 func modulo_distributive(a, b, c int) int {
-	return (a % c) * (b % c) % c
+	if b == 1 {
+		return a % c
+	} else if b%2 == 0 {
+		tmp := modulo_distributive(a, b/2, c)
+		return (tmp % c) * (tmp % c) % c
+	}
+	return modulo_distributive(a, b/2, c) * modulo_distributive(a, (b/2)+1, c) % c
 }
